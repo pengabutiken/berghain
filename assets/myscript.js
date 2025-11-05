af048e235f7cfc70808189f401066122fe151044
Theo Björkqvist <113356799+pengabutiken@users.noreply.github.com>
2025-11-05 22:59:38 +0100

    lag till höj o sänk knappar och stylat dem men ingen funktion än! pasteat lyrics med

--- assets/myscript.js
+++ assets/myscript.js
@@ -1,10 +1,17 @@
 const audio=document.getElementById("song");
 const playB=document.getElementById("play")
+const higher=document.getElementById("higher");
+const lower=document.getElementById("lower");
+
 playB.addEventListener("click", function(){
 console.log("hej");
     if(audio.paused){
     audio.play();
     playB.style.display="none";
+
+    lower.style.display="block";
+    higher.style.display="block";
+
 }
 
 });
